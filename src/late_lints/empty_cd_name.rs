c8add5782a3cbf067b338b1d8060bc3b7342e380
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2024-02-11 14:10:54 +0000

    chore: cleanup debug logging

--- src/late_lints/empty_cd_name.rs
+++ src/late_lints/empty_cd_name.rs
@@ -1,5 +1,5 @@
 use ariadne::{Color, Label, Report, ReportKind};
-use tracing::*;
+
 
 use crate::config::Config;
 use crate::custom_difficulty::CustomDifficulty;
@@ -10,10 +10,7 @@ pub fn lint_empty_cd_name<'a>(
     path: &'a String,
     diag: &mut Vec<Report<'a, (&'a String, std::ops::Range<usize>)>>,
 ) {
-    debug!("{:#?}", cd);
-
     if cd.name.val.is_empty() {
-        debug!(cd_name_span = ?cd.name.span);
         diag.push(
             Report::build(ReportKind::Warning, path, cd.name.span.start)
                 .with_message("custom difficulty name is empty")
